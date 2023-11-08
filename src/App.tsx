740fe9cdc1b28e84bcd15c06e5dc3a3bfa2d8840
(luqman syahreno) <eno.luqman@gmail.com>
2023-11-08 21:51:44 +0700

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import './globals.css';
+import { Routes, Route } from "react-router-dom";
+
+import "./globals.css";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Snapgram
-    </h1>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
 
-export default App
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
